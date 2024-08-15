5b9f4b5f264c00883b0c51f78d860ac4d2cf2182
wvyrbnyq3t <sota.kanto.9x@stu.hosei.ac.jp>
2024-08-15 22:55:45 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ $('#js-addBtn').click(async function(){
   ,   memo = $('#js-schedule-memo').val();
 
   if(date !== "" && ttl !== ""){
-    await fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ date: date, ttl: ttl, memo: memo }) })
+    await fetch('/api/user', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ date: date, ttl: ttl, memo: memo }) });
     alert('登録しました');
 
     // reload
